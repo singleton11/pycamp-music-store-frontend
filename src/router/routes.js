96ce911ade9af165312b6b6f803eeb78300a6d5e
Denis Dontsov <de-don@mail.ru>
2018-04-25 14:33:49 +0700

    Decorators for routing have been added.
    The decorator ifNotAuthenticated check that the user is NOT authorized, otherwise user will be redirected it on the dashboard.
    The decorator ifAuthenticated check that the user is authorized, otherwise user will be redirected it on the login page.

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,15 +2,41 @@ import LoginForm from '@/components/LoginForm'
 import Home from '@/components/Home'
 import Dashboard from '@/components/Dashboard'
 
+import store from '@/store/store'
+import { AUTHENTICATED_GETTER } from '@/store/getter-types'
+
 export const HOME_PAGE = '/'
 export const LOGIN_PAGE = '/login'
 export const DASHBOARD_PAGE = '/dashboard'
+
+/**
+ * Router Decorator: If user is not authenticated
+ */
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters[AUTHENTICATED_GETTER]) {
+    next()
+    return
+  }
+  next(DASHBOARD_PAGE)
+}
+
+/**
+ * Router Decorator: If user is authenticated
+ */
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters[AUTHENTICATED_GETTER]) {
+    next()
+    return
+  }
+  next(LOGIN_PAGE)
+}
 
 export default [
   {
     path: LOGIN_PAGE,
     name: 'login',
-    component: LoginForm
+    component: LoginForm,
+    beforeEnter: ifNotAuthenticated
   },
   {
     path: HOME_PAGE,
@@ -20,6 +46,7 @@ export default [
   {
     path: DASHBOARD_PAGE,
     name: 'dashboard',
-    component: Dashboard
+    component: Dashboard,
+    beforeEnter: ifAuthenticated
   }
 ]
