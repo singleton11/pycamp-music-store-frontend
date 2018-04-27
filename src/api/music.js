c5ca50379d6e6558db98468c319b8bf146429b5a
Denis Dontsov <de-don@mail.ru>
2018-04-27 08:02:51 +0700

    Fixed file music.js for one export

--- src/api/music.js
+++ src/api/music.js
@@ -1,96 +1,87 @@
 import axios from './axios'
 
-/**
- * Get track info by id
- *
- * @param {String} trackId - track id
- * @returns {Promise}
- */
-export function getTrack (trackId) {
-  return axios.get('music_store/tracks/' + trackId + '/')
-}
-
-/**
- * Get all tracks
- *
- * @param {String} search - string for searching
- * @returns {Promise}
- */
-export function getTracks (search = '') {
-  let q = ''
-  if (search) {
-    q = '?search=' + search
-  }
-  return axios.get('music_store/tracks/' + q)
-}
-
-/**
- * Get all bought tracks
- *
- * @returns {Promise}
- */
-export function getBoughtTracks () {
-  return axios.get('music_store/bought_tracks/')
-}
-
-/**
- * Buy track by id
- *
- * @returns {Promise}
- */
-export function buyTrack (trackId) {
-  return axios.post('music_store/tracks/' + trackId + '/buy/')
-}
-
-/**
- * Get album info by id
- *
- * @param {String} albumId - album id
- * @returns {Promise}
- */
-export function getAlbum (albumId) {
-  return axios.get('music_store/albums/' + albumId + '/')
-}
-
-/**
- * Get all albums
- *
- * @param {String} search - string for searching
- * @returns {Promise}
- */
-export function getAlbums (search = '') {
-  let q = ''
-  if (search) {
-    q = '?search=' + search
-  }
-  return axios.get('music_store/albums/' + q)
-}
-
-/**
- * Get all bought albums
- *
- * @returns {Promise}
- */
-export function getBoughtAlbums () {
-  return axios.get('music_store/bought_albums/')
-}
-
-/**
- * Buy album by id
- *
- * @returns {Promise}
- */
-export function buyAlbum (albumId) {
-  return axios.post('music_store/albums/' + albumId + '/buy/')
-}
-
 export default {
-  getTrack,
-  getTracks,
-  getBoughtTracks,
-  buyTrack,
-  getAlbum,
-  getAlbums,
-  getBoughtAlbums,
-  buyAlbum
+  /**
+   * Get track info by id
+   *
+   * @param {String} trackId - track id
+   * @returns {Promise}
+   */
+  getTrack (trackId) {
+    return axios.get('music_store/tracks/' + trackId + '/')
+  },
+
+  /**
+   * Get all tracks
+   *
+   * @param {String} search - string for searching
+   * @returns {Promise}
+   */
+  getTracks (search = '') {
+    let q = ''
+    if (search) {
+      q = '?search=' + search
+    }
+    return axios.get('music_store/tracks/' + q)
+  },
+
+  /**
+   * Get all bought tracks
+   *
+   * @returns {Promise}
+   */
+  getBoughtTracks () {
+    return axios.get('music_store/bought_tracks/')
+  },
+
+  /**
+   * Buy track by id
+   *
+   * @returns {Promise}
+   */
+  buyTrack (trackId) {
+    return axios.post('music_store/tracks/' + trackId + '/buy/')
+  },
+
+  /**
+   * Get album info by id
+   *
+   * @param {String} albumId - album id
+   * @returns {Promise}
+   */
+  getAlbum (albumId) {
+    return axios.get('music_store/albums/' + albumId + '/')
+  },
+
+  /**
+   * Get all albums
+   *
+   * @param {String} search - string for searching
+   * @returns {Promise}
+   */
+  getAlbums (search = '') {
+    let q = ''
+    if (search) {
+      q = '?search=' + search
+    }
+    return axios.get('music_store/albums/' + q)
+  },
+
+  /**
+   * Get all bought albums
+   *
+   * @returns {Promise}
+   */
+  getBoughtAlbums () {
+    return axios.get('music_store/bought_albums/')
+  },
+
+  /**
+   * Buy album by id
+   *
+   * @returns {Promise}
+   */
+  buyAlbum (albumId) {
+    return axios.post('music_store/albums/' + albumId + '/buy/')
+  }
 }
