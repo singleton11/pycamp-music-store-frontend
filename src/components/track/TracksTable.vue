5217474aeb1a2f61ed79a01afba5ce7bd7e7a17d
Denis Dontsov <de-don@mail.ru>
2018-04-28 16:14:19 +0700

    Added a message `Nothing to show` in TracksTable component
    Added a message, which displayed in the TracksTable component, when nothing is found.

--- src/components/track/TracksTable.vue
+++ src/components/track/TracksTable.vue
@@ -1,25 +1,30 @@
 <template>
-  <table class="table table-hover">
-    <thead>
-    <tr>
-      <th scope="col">Author</th>
-      <th scope="col">Title</th>
-      <th scope="col">Price</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="track in tracks"
-        v-bind:key="track.id"
-        @click.prevent="$emit('select', track)"
-        :class="[track.is_bought ? 'table-success' : 'table-primary']">
-      <td scope="col">{{track.author}}</td>
-      <th scope="col">
-          {{track.title}}
-      </th>
-      <td scope="col">{{track.price}}</td>
-    </tr>
-    </tbody>
-  </table>
+  <div>
+    <table class="table table-hover" v-if="tracks.length">
+      <thead>
+      <tr>
+        <th scope="col">Author</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="track in tracks"
+          v-bind:key="track.id"
+          @click.prevent="$emit('select', track)"
+          :class="[track.is_bought ? 'table-success' : 'table-primary']">
+        <td scope="col">{{track.author}}</td>
+        <th scope="col">
+            {{track.title}}
+        </th>
+        <td scope="col">{{track.price}}</td>
+      </tr>
+      </tbody>
+    </table>
+    <blockquote class="blockquote text-center" v-else>
+      <p class="mb-0">Nothing to show</p>
+    </blockquote>
+  </div>
 
 </template>
 
