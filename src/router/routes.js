e39f24422332462b5cc0e72b169c222ef5c6cd2c
Denis Dontsov <de-don@mail.ru>
2018-04-28 10:07:08 +0700

    Added the checking authorization checking during routing

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,44 @@
 import LoginForm from '../components/LoginForm.vue';
 import Home from '../components/Home.vue';
 import PagePayments from '../components/PagePayments.vue';
+import store from '../store';
 
 export const HOME_PAGE = '/';
 export const LOGIN_PAGE = '/login';
 export const PAYMENTS_PAGE = '/payments';
+
+
+/**
+ * Router checker: If user is authenticated
+ */
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+
+    return;
+  }
+  next({ name: 'login', });
+};
+
+
+/**
+ * Router checker: If user is not authenticated
+ */
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+
+    return;
+  }
+  next({ name: 'payments', });
+};
 
 export default [
   {
     path: LOGIN_PAGE,
     name: 'login',
     component: LoginForm,
+    beforeEnter: ifNotAuthenticated,
   },
   {
     path: HOME_PAGE,
@@ -21,5 +49,6 @@ export default [
     path: PAYMENTS_PAGE,
     name: 'payments',
     component: PagePayments,
+    beforeEnter: ifAuthenticated,
   },
 ];
