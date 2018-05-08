fe33506575d6c1b34c9f21b1a50da6952f5148a9
de-don <de-don@mail.ru>
2018-05-08 07:24:06 +0700

    Added global Event Bus
    This is mechanism for communicating between the components through events

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias
 import Vue from 'vue';
 import store from './store/index';
 import router from './router/router';
 import App from './App.vue';
+
+Vue.prototype.$eventHub = new Vue(); // global Event Bus
 
 /* eslint-disable-next-line */
 new Vue({
